df17bd1543780c03ebe7b1cf0bb0b24c26adcc75
Felipe Padovani <padovani1010@gmail.com>
2024-03-18 10:38:12 -0300

    gradient behind image

--- src/components/ShoeDetail.tsx
+++ src/components/ShoeDetail.tsx
@@ -4,13 +4,16 @@ function ShoeDetail() {
     <div className="flex flex-col lg:flex-row-reverse space-y-4">
       {/* Image */}
       <div className="flex-1">
-        <img src={nike1} alt="" />
+        <div className='bg-gradient-to-br from-[#F637CF] from-5% via-[#E3D876] via-40% to-[#4DD4C6]'>
+          <img src={nike1}/>
+        </div>
       </div>
       {/* Description */}
       <div className="flex-1 space-y-6">
         <h1 className="text-5xl font-black md:text-9xl">Nike Air Max 270</h1>
         <p className="font-medium md:text-xl">{`The Nike Air Max 270 is a lifestyle shoe that's sure to turn heads with its vibrant color gradient.`}</p>
         <h2 className="text-3xl font-extrabold md:text-6xl">160 $</h2>
+        {/* buttoms and links */}
         <div className="space-x-10">
           <button className="w-44 h-14 bg-black text-white hover:bg-gray-900 active:bg-gray-700">
             Add to chart
