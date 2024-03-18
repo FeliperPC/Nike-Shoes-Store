af222eebf656335abedcdbd70a9d0aecf2905fcb
Felipe Padovani <padovani1010@gmail.com>
2024-03-18 09:42:50 -0300

    header menu responsive done

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Nav from "./components/Nav";
 function App() {
   return (
     <div className="p-10 xl:px-24">
-        <Nav/>
+      <Nav/>
     </div>
   );
 }
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,19 +1,48 @@
-import NikeLogo from '../assets/nike-logo.svg?react'
+import { TbShoppingBag } from "react-icons/tb";
+import NikeLogo from "../assets/nike-logo.svg?react";
 import { RxHamburgerMenu } from "react-icons/rx";
+import { useState } from "react";
 
 function Nav() {
-  return(
-    <nav className='flex items-center justify-between'>
+  const [isMobile,setIsMobile] = useState(false);
+  const ROUTES = ["Home", "About", "Services", "Pricing", "Contact"];
+  return (
+    <nav className="flex flex-wrap items-center justify-between">
       {/* logo, throws nowhere */}
-      <a href="#"> 
-        <NikeLogo className='h-20 w-20'/>
-      </a>  
+      <a href="#">
+        <NikeLogo className="h-20 w-20" />
+      </a>
       {/* burguerButton */}
-        <button className='hover:bg-gray-100 p-2 focus:ring-2 rounded-lg focus:ring-gray-200'>
-          <RxHamburgerMenu size={25}/>
-        </button>
+      <button className="lg:hidden hover:bg-gray-100 p-2 focus:ring-2 rounded-lg focus:ring-gray-200"
+        onClick={()=>setIsMobile(!isMobile)}
+      >
+        {/* Thinking on mobile first the buttom on larger screens should not appear */}
+        <RxHamburgerMenu size={25} />
+      </button>
+      {/* Menu list */}
+      <div className={`${!isMobile && 'hidden'} w-full lg:w-auto p lg:block`}>
+        <ul className="lg:space-x-8 flex flex-col lg:flex-row lg:bg-transparent bg-gray-50 lg:border-none text-lg border-gray-100 rounded-lg p-4 cursor-pointer">
+          {/* li has display block., no need to insert display column */}
+          {ROUTES.map((item, i) => (
+            <li
+              className={`px-3 py-2 rounded ${i === 0 ? "bg-blue-500 lg:bg-transparent lg:text-blue-500 text-white" : "hover:bg-gray-100"}`}
+              key={item}
+            >
+              {item}
+            </li>
+          ))}
+        </ul>
+      </div>
+      {/* Thinking on mobile first this looks great ! */}
+      <div className="fixed left-4 bottom-4 lg:static">
+        {/* if you put position fixed, to change that to default again put static to other screens*/}
+        <div className="flex-center h-12 w-12 rounded-full bg-white shadow-md">
+          {/* Cart buttom */}
+          <TbShoppingBag />
+        </div>
+      </div>
     </nav>
-  )
+  );
 }
 
-export default Nav
+export default Nav;
