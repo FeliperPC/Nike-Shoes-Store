5813b6788198548337724446c66c6816fe45a3d4
Felipe Padovani <padovani1010@gmail.com>
2024-03-21 15:00:38 -0300

    correcting errors to deploy 1

--- src/redux/index.tsx
+++ src/redux/index.tsx
@@ -1,5 +1,4 @@
 import { legacy_createStore as createStore } from "redux";
-import { composeWithDevTools } from "@redux-devtools/extension"
 import { combineReducers } from "redux";
 import sideBarReducer from "./reducers/sideBarReducer";
 import cartReducer from "./reducers/cartReducer";
@@ -7,5 +6,5 @@ import previewReducer from "./reducers/previewReducer";
 
 const rootReducer = combineReducers({sideBarReducer,cartReducer,previewReducer})
 
-export const store = createStore(rootReducer,composeWithDevTools());
+export const store = createStore(rootReducer);
 
